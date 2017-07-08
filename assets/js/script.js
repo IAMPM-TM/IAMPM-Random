4b5cff0d2bb2544110b85ab56aa473d42b3f1e8e
cyberzass <cyberzass@gmail.com>
2017-07-08 14:24:31 +0300

    combined logic with markup

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 class Chance {
   constructor(fail) {
     this.fail = fail;
-    this.success = 100 - fail;
+    this.win = 100 - fail;
   }
 }
 
@@ -60,6 +60,42 @@ class Randomizer {
 }
 
 const rand = new Randomizer();
-const chance = rand.getChance(1);
 
-console.log(chance);
+function calculate() {
+  const teamLevel = parseInt(document.getElementById("team-level").value);
+  const result = rand.getChance(teamLevel);
+
+  if (result) {
+
+    setResult(result);
+    hideResult();
+    showDice();
+
+    setTimeout(function() {
+      hideDice();
+      showResult();  
+    }, 3000);
+
+  }
+}
+
+function setResult(result) {
+  document.getElementById("fail").innerText = result.fail + "%";
+  document.getElementById("win").innerText = result.win + "%";
+}
+
+function showDice() {
+  document.getElementById("dice").style.display = "block";
+}
+
+function hideDice() {
+  document.getElementById("dice").style.display = "none";
+}
+
+function showResult() {
+  document.getElementById("result").style.display = "flex";
+}
+
+function hideResult() {
+  document.getElementById("result").style.display = "none";
+}
